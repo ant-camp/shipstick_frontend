0490f41aa3045a9ce4208f5df0d76828610f55e7
Anthony <anttyaadiggg@yahoo.com>
2023-11-21 15:17:24 -0500

    Add product api service

---
+++ src/services/ProductsService.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+// Create the API client
+const api = axios.create({
+  baseURL: 'http://localhost:3001/',
+});
+
+//need to test this
+const productService = {
+  // Fetch a single product
+  getProduct: async (length, width, height, weight) => {
+    try {
+      const response = await api.get('/products/find_by_dimensions', {
+        params: { length, width, height, weight } 
+      });
+      return response.data;
+    } catch (error) {
+      // Handle error
+      console.error("Error fetching product:", error);
+      throw error;
+    }
+  },
+
+};
+
+export default productService;
