d654998440fbc4ecdcddffebd1fc97a1b70beb55
Anthony <anttyaadiggg@yahoo.com>
2023-12-01 12:49:11 -0500

    Minor api change to responses

--- src/services/ProductsService.js
+++ src/services/ProductsService.js
@@ -5,22 +5,33 @@ const api = axios.create({
   baseURL: 'http://localhost:3001/',
 });
 
-//need to test this
 const productService = {
   // Fetch a single product
   getProduct: async (length, width, height, weight) => {
     try {
       const response = await api.get('/products/find_by_dimensions', {
-        dimension_params: { length, width, height, weight } 
+        params: {
+          length, 
+          width, 
+          height, 
+          weight
+        }
       });
+
       return response.data;
     } catch (error) {
       // Handle error
-      console.error("Error fetching product:", error);
-      throw error;
+      if (error.response && error.response.status === 404) {
+        // If the error is 404
+        console.error("Error fetching product:", error.response.data.error);
+        return { error: error.response.data.error };
+      } else {
+        // Other Errors
+        console.error("Error fetching product:", error.message || "An error occurred");
+        throw error;
+      }
     }
   },
-
 };
 
 export default productService;
